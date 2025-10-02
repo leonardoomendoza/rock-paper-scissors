1b1f68a28a79ec57c3be0de3be6904d2f89fae7b
leonardoomendoza <jmendoza42@angelo.edu>
2025-10-02 14:11:08 -0500

    both functions work, and playRound returns human choice as lowercase

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 function getComputerChoice() {
-    let randNum = Math.random(3);
+    let randNum = Math.floor(Math.random() * 3);
     if (randNum === 0) {
         return 'rock'
     }
     else if (randNum === 1) {
         return 'paper';
     }
-    else {
+    else if (randNum === 2) {
         return 'scissors';
     }
 }
+function getHumanChoice() {
+    let humanChoice = prompt('rock, paper, or scissors:  ');
+    return humanChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+
+function playRound(humanChoice , computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    return humanChoice + " " + computerChoice;
+}
+
+
+
+
+
