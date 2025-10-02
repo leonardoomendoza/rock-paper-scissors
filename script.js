d5b7281421e0e486bc21d493c576536fcde49d01
leonardoomendoza <jmendoza42@angelo.edu>
2025-10-01 20:23:51 -0500

    added getComputerChoice function with three conditions

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let randNum = Math.random(3);
+    if (randNum === 0) {
+        return 'rock'
+    }
+    else if (randNum === 1) {
+        return 'paper';
+    }
+    else {
+        return 'scissors';
+    }
+}
