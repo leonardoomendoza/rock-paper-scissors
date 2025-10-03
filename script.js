5ba9b097f84b9c4408454bd7cd287c0b1daa23fb
leonardoomendoza <jmendoza42@angelo.edu>
2025-10-02 20:20:08 -0500

    playRound function is working now, game logic is fixed, with different conitional statements.

--- script.js
+++ script.js
@@ -18,15 +18,64 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
 
 
 
-function playRound(humanChoice , computerChoice) {
+
+function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
-    return humanChoice + " " + computerChoice;
+    // let humanWon = true;
+    // let tie = false;
+    
+    if (humanChoice === computerChoice) {
+        return "you tied";
+    }
+
+    else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        humanScore++;
+        return `you won ${humanChoice} beats ${computerChoice}`;
+    } 
+    
+    else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        humanScore++;
+        return `you won ${humanChoice} beats ${computerChoice}`;
+    }
+    else if (humanChoice == 'paper' && computerChoice === 'rock') {
+        humanScore++;
+        return `you won ${humanChoice} beats ${computerChoice}`;
+    }
+   
+    else {
+        computerScore++;
+        return `you lost ${computerChoice} beats ${humanChoice}`;
+    }
+
+    // if(tie) {
+    //     console.log("you tied");
+    //     console.log(`computer: ${computerSelection} human: ${humanSelection}`);
+    // }
+    // else if(humanWon) {
+    //     console.log("you won");
+    //     console.log(`computer: ${computerSelection} human: ${humanSelection}`);
+    // }
+    // else {
+    //     console.log("you lost");
+    //     console.log(`computer: ${computerSelection} human: ${humanSelection}`);
+    // }
+
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
